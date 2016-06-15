ade6bc0e6edd211b2681f8366badf7ab594b3e51
Kimmy Leo <kenpusney@gmail.com>
2016-06-16 02:09:05 +0800

    execution and logging

--- client/src/main/java/net/kimleo/grabbie/client/component/TaskService.java
+++ client/src/main/java/net/kimleo/grabbie/client/component/TaskService.java
@@ -3,6 +3,8 @@ package net.kimleo.grabbie.client.component;
 import net.kimleo.grabbie.client.agent.AgentInfo;
 import net.kimleo.grabbie.component.Navigator;
 import net.kimleo.grabbie.model.Execution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
@@ -10,11 +12,18 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
-
+    public static final Logger LOGGER = LoggerFactory.getLogger(TaskService.class);
     @Autowired
     Navigator navigator;
 
@@ -28,8 +37,20 @@ public class TaskService {
     public void taskExecutionLoop() {
         Execution[] execs = getNotExecutedTasks();
         for (Execution exec : execs) {
-            System.out.println(exec);
+            LOGGER.info("Execution found: {}", exec);
             String ExecUrl = navigator.execution(exec.getId());
+            ArrayList<String> command = new ArrayList<>();
+            command.add(exec.getTask().getCommand());
+            command.addAll(Arrays.asList(exec.getTask().getArgs()));
+            try {
+                Process process = new ProcessBuilder(command).start();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String result = reader.lines().collect(Collectors.joining("\n"));
+                exec.setResult(result);
+                LOGGER.info("Executed result: {}", result);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             exec.setExecuted(true);
             RequestEntity<Execution> requestEntity = RequestEntity
                     .put(URI.create(ExecUrl))
